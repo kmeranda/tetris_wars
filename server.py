66a1124c85ef55cdd0c5a7f266ad628e46c15c5e
kforbes <kforbes@nd.edu>
2016-05-02 14:09:38 -0400

    sends initial board array to other player and displays

--- server.py
+++ server.py
@@ -8,11 +8,17 @@ from twisted.internet.protocol import Protocol
 from twisted.protocols.basic import LineReceiver
 from twisted.internet.tcp import Port
 from twisted.internet import reactor
+from twisted.internet.defer import DeferredQueue
 
 
 #init port number
 PLAYER1_PORT = 40011
 PLAYER2_PORT = 40111
+
+
+#init queues
+P1_QUEUE = DeferredQueue()
+P2_QUEUE = DeferredQueue()
 
 
 ## player 1 ##
@@ -24,6 +30,10 @@ class Player1Connection(LineReceiver): #connects server
 		reactor.listenTCP(PLAYER2_PORT, Player2ConnFactory())
 	def dataReceived(self, data):
 		print "Received Data"#, data -- prints too much garbage
+		P2_QUEUE.put(data)
+		P1_QUEUE.get().addCallback(self.sendData)
+	def sendData(self, data): #send data to player 1
+		self.transport.write(data)
 	def connectionLost(self, reason):
 		print "Connection lost from Player 1:", self.addr
 
@@ -41,6 +51,10 @@ class Player2Connection(LineReceiver):
 		print "Connection received from Player 2:", self.addr
 	def dataReceived(self, data):
 		print "Received Data"#, data -- prints too much garbage
+		P1_QUEUE.put(data)
+		P2_QUEUE.get().addCallback(self.sendData)
+	def sendData(self, data): #send data to player 2
+		self.transport.write(data)
 	def connectionLost(self, reason):
 		print "Connection lost from Player 2:", self.addr
 
